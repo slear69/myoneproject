b45ceebe9188d7c153e852864b8b4070d7f9604b
hi_frombg:)ngl <dimitarp758@gmail.com>
2026-01-22 09:38:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("anketa")
 text = st.text_input("vavedi lobimo shivotno")
-subject = st.selectbox("животни
-[text , "куче"]")
+subject = st.selectbox("животни",
+[text , "куче","котка"])
